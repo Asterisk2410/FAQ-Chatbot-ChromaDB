758622495944e0217ce28930526c4a61ce54fd13
Asterisk2410 <nimit19bhinde98@gmail.com>
2024-05-14 17:09:48 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'FAQ Chatbot',
+    version= '0.0.1',
+    author= 'Nimit Bhinde',
+    author_email= 'nabhinde"gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
