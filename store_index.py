69aaeede7289d9c02c33526369c98ad090af3d60
Asterisk2410 <nimit19bhinde98@gmail.com>
2024-05-15 13:16:48 +0530

    if condition for indexing the vector db

--- store_index.py
+++ store_index.py
@@ -1,18 +1,26 @@
 from langchain.vectorstores import chroma
 from src.helper import load_pdf, text_split, download_hugging_face_embeddings
 from dotenv.main import load_dotenv
+from logs.logger import get_logger
 import os
 
 load_dotenv()
 
-PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
-dataset = r"C:\Users\Admin\Desktop\Aster\Chatbot-Llama2\data"
+logger = get_logger(__name__)
+
+# PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+dataset = r"/FAQ Chatbot-ChromaDB/data"
 
 extracted_data = load_pdf(dataset)
 text_chunks = text_split(extracted_data)
 embeddings = download_hugging_face_embeddings()
 index_name = "chat"
+directory = r"/FAQ Chatbot-ChromaDB/chat"
 
-vectordb = chroma.Chroma.from_documents(documents=text_chunks, embedding=embeddings, persist_directory=index_name)
-
-vectordb.persist()
+if not os.path.exists(directory):
+    logger.info("Creating vector database")
+    vectordb = chroma.Chroma.from_documents(documents=text_chunks, embedding=embeddings, persist_directory=index_name)
+    vectordb.persist()
+else:
+    logger.info("vector database already exists")
+    pass
