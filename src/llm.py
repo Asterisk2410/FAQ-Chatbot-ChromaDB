0b1d417a38a7a3949603520364d944cfb5c25090
Asterisk2410 <nimit19bhinde98@gmail.com>
2024-06-06 19:13:40 +0530

    llama cpp + cuda commits DND code

--- src/llm.py
+++ src/llm.py
@@ -1,12 +1,5 @@
 import sys , time, torch, numpy
-
-from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.base import BaseCallbackHandler
-from langchain.chains.llm import LLMChain
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-from langchain.llms import llamacpp
-from langchain.prompts import PromptTemplate
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
